59f730a49a780938666e5d6eb08479576a229443
Craig Douglas French <craigdfrench@users.noreply.github.com>
2019-09-23 11:18:21 -0400

    First crack at REST API with FixedDataTable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <FixedDataTable></FixedDataTable>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/FixedDataTable.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Table, Column, Cell} from 'fixed-data-table-2';
+import 'fixed-data-table-2/dist/fixed-data-table.css';
+
+var rows = []
+
+function getJSONData() {
+    fetch("http://localhost:8080/event")
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data);
+        rows = data;
+        ReactDOM.render(<FixedDataTableDemo/>, document.getElementById('root'));
+    })
+}
+
+getJSONData();
+
+const FixedDataTableDemo = () => 
+  <Table
+    rowHeight={50}
+    rowsCount={rows.length}
+    width={500}
+    height={500}
+    headerHeight={50}>
+    <Column
+      header={<Cell>ID</Cell>}
+      columnKey = 'ID'
+      cell={({rowIndex, columnKey, ...props}) => (
+        <Cell {...props}>
+          {rows[rowIndex][columnKey]}
+        </Cell>
+      )}
+      width={200}
+    />
+    <Column
+      header={<Cell>Email</Cell>}
+      columnKey = 'Email'
+      cell={({rowIndex, columnKey, ...props}) => (
+        <Cell {...props}>
+          {rows[rowIndex][columnKey]}
+        </Cell>
+      )}
+      width={200}
+    />
+    <Column
+      header={<Cell>Component</Cell>}
+      columnKey = 'Component'
+      cell={({rowIndex, columnKey, ...props}) => (
+        <Cell {...props}>
+          {rows[rowIndex][columnKey]}
+        </Cell>
+      )}
+      width={200}
+    />
+    <Column
+      header={<Cell>Environment</Cell>}
+      columnKey = 'Environment'
+      cell={({rowIndex, columnKey, ...props}) => (
+        <Cell {...props}>
+          {rows[rowIndex][columnKey]}
+        </Cell>
+      )}
+      width={200}
+    />
+    <Column
+      header={<Cell>Message</Cell>}
+      columnKey = 'Message'
+      cell={({rowIndex, columnKey, ...props}) => (
+        <Cell {...props}>
+          {rows[rowIndex][columnKey]}
+        </Cell>
+      )}
+      width={200}
+    />
+    <Column
+      header={<Cell>Data</Cell>}
+      columnKey = 'Data'
+      cell={({rowIndex, columnKey, ...props}) => (
+        <Cell {...props}>
+          {rows[rowIndex][columnKey]}
+        </Cell>
+      )}
+      width={200}
+    />
+  </Table> 
+
+export default FixedDataTableDemo;
